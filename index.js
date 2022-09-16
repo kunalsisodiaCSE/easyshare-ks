904a85fcb14eb5df5e8d515ac8f4d6a83c1ab98c
Kunal Sisodia <kunal.12014621@lpu.in>
2022-09-17 01:19:12 +0530

    JS update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://easyshare-kunal.herokuapp.com";
+const baseURL = "https://easyshare-kunal.herokuapp.com/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
